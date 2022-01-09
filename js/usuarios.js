a0759a05a6313a7de6a2a729ab67775c01287b9f
Angel Molina <84240337+AlexReyes9725@users.noreply.github.com>
2022-01-09 14:29:01 -0500

    Correcion usuarios
    se modifico el guardar datos del usuario

--- js/usuarios.js
+++ js/usuarios.js
@@ -3,21 +3,19 @@ var datos_usuario = new Array();
 
 function guardar_datos_usuario(nombre, apellido, email, telefono, direccion,cedula) {
     datos_usuario = new Array(nombre, apellido, email, telefono, direccion, cedula);
-    /*datos_usuario = JSON.stringify(datos_usuario)
+    datos_usuario = JSON.stringify(datos_usuario)
     datos = new Array();
     if(localStorage.getItem("datos_usuario")!=null){
         datos = localStorage.getItem("datos_usuario");
     }
     datos += datos_usuario+"|";
-    localStorage.setItem("datos_usuario", datos);*/
-    localStorage.setItem("datos_usuario", JSON.stringify(datos_usuario));
+    localStorage.setItem("datos_usuario", datos);
 }
 
 function obtener_datos_usuario() {
-    var datos_usuario = JSON.parse(localStorage.getItem("datos_usuario"));
-    /*var midato = datos_usuario.split("|");
-    return midato;*/
-    return datos_usuario;
+    var datos_usuario = localStorage.getItem("datos_usuario");
+    var midato = datos_usuario.split("|");
+    return midato;  
 }
 
 function login(email, cedula){
