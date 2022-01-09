829cbea4eb8feee8f448b9924b7489f96b753c94
Angel Molina <84240337+AlexReyes9725@users.noreply.github.com>
2022-01-09 00:53:57 -0500

    commit de js
     modificación del archivo de reserva.js

---
+++ js/reserva.js
@@ -0,0 +1,52 @@
+var valor_aPagar = 0
+//VARIABLES DE PRECIO DE MASAJES
+var MasajeN = 9.99;
+var MasajeV = 14.99;
+//VARIABLES DE PRECIO DE DEPILACIONES
+var DepilacionN = 14.99
+var DepilacionV = 19.99
+//VARIABLES DE PRECIO DE FACIAL
+var FacialN = 19.99
+var FacialV = 24.99
+
+function Registrar_reserva(servicio, tipo_servicio, fecha, hora, sucursal, total){
+    datos_reserva = new Array (servicio, tipo_servicio, fecha, hora, sucursal, total)
+    localStorage.setItem("datos_reserva", JSON.stringify(datos_reserva));
+}
+
+function obtener_datos_reserva() {
+    var datos_reserva = JSON.parse(localStorage.getItem("datos_reserva"));
+    return datos_reserva;
+}
+
+function Precio(servicio,tipo_servicio){
+    if(tipo_servicio == "normal"){
+        if(servicio == "Masaje"){
+            valor_aPagar = MasajeN + (MasajeN*0.12)
+            return valor_aPagar
+
+        }else if (servicio == "Depilacion"){
+            valor_aPagar = DepilacionN + (DepilacionN*0.12)
+            return valor_aPagar
+
+        }else if (servicio == "Facial"){
+            valor_aPagar = FacialN + (FacialN*0.12)
+            return valor_aPagar
+        }
+
+    }else if(tipo_servicio == "VIP"){
+        if(servicio == "Masaje"){
+            valor_aPagar = MasajeV + (MasajeV*0.12)
+            return valor_aPagar
+
+        }else if (servicio == "Depilacion"){
+            valor_aPagar = DepilacionV + (DepilacionV*0.12)
+            return valor_aPagar
+
+        }else if (servicio == "Facial"){
+            valor_aPagar = FacialV + (FacialV*0.12)
+            return valor_aPagar
+
+        }
+    }
+}
